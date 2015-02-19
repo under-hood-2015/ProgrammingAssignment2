279a93d63b43136ce5718840c9d12b2099f0d095
under.hood <under_hood_2015@hmamail.com>
2015-02-19 21:42:20 +0100

    publishing hw3 to github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# this is part of an online course from coursera.org
+# it's called R programming, in short rprog-011
+#
+# this is assignement 2 
+#
+# the following functions provide functionality to solve a matrix and
+# additionaly cache the result to prevent doing the 
+# same computation again and again
 
-## Write a short comment describing this function
 
+#
+#  -  makeCacheMatrix: This function creates a special 
+#    "matrix" object that can cache its inverse.
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(solve) m <<- solve
+  getsolved <- function() m
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved)
 }
 
 
-## Write a short comment describing this function
-
+#  - cacheSolve: This function computes the inverse 
+#    of the special "matrix" returned by makeCacheMatrix above. 
+#    If the inverse has already been calculated 
+#    (and the matrix has not changed), then cacheSolve should 
+#    retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolved()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolved(m)
+  m
 }
